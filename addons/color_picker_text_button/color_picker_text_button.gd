d2eab0add4b64be7b003d31f0db8a4be602b6ae4
mooocow124 <shauryachhabra914@gmail.com>
2024-04-04 15:06:05 +0530

    Revamp for Godot 4 + add font size

--- addons/color_picker_text_button/color_picker_text_button.gd
+++ addons/color_picker_text_button/color_picker_text_button.gd
@@ -1,21 +1,22 @@
-tool
+@tool
 extends Button
 
 signal popup_hide
 signal color_changed(color)
 
-export var text_value = "Click Me" setget set_label_text
-export(StyleBoxFlat) var style setget set_styles
-export(float, 0.1, 0.8) var mouseover_darkening_factor = 0.1
-export(DynamicFont) var custom_font setget set_font
-export(Color) var color = Color.white setget set_colors_in_tool
-export(Color) var light_text_color = Color.white setget update_light_text_color
-export(Color) var dark_text_color = Color.black setget update_dark_text_color
-export(float, 0.2, 0.8) var text_color_flip_threshold = 0.5
-export var popup_centered = true
-export var simple_color_picker = true
-export var enable_text = true setget show_hide_label
-export var fresh = true
+@export var text_value = "Click Me": set = set_label_text
+@export var style: StyleBoxFlat: set = set_styles
+@export var mouseover_darkening_factor = 0.1 # (float, 0.1, 0.8)
+@export var custom_font: FontFile: set = set_font
+@export var color: Color = Color.WHITE: set = set_colors_in_tool
+@export var light_text_color: Color = Color.WHITE: set = update_light_text_color
+@export var dark_text_color: Color = Color.BLACK: set = update_dark_text_color
+@export var text_color_flip_threshold = 0.5 # (float, 0.2, 0.8)
+@export var popup_centered = true
+@export var simple_color_picker = true
+@export var enable_text = true: set = show_hide_label
+@export var fresh = true
+@export var font_size = 20
 
 var panel
 var color_picker
@@ -32,10 +33,10 @@ func _ready():
 	panel.add_child(color_picker)
 
 	# Connect signals
-	var _e = connect("pressed", self, "show_panel")
-	_e = color_picker.connect("color_changed", self, "set_colors")
-	_e = panel.connect("popup_hide", self, "panel_closed")
-	_e = connect("item_rect_changed", self, "rect_changed")
+	var _e = connect("pressed", Callable(self, "show_panel"))
+	_e = color_picker.connect("color_changed", Callable(self, "set_colors"))
+	_e = panel.connect("popup_hide", Callable(self, "panel_closed"))
+	_e = connect("item_rect_changed", Callable(self, "rect_changed"))
 
 
 	if simple_color_picker:
@@ -44,12 +45,12 @@ func _ready():
 			color_picker.get_child(n).hide()
 
 	# Make panel resize to fit contents
-	panel.rect_size.y = 0
+	panel.size.y = 0
 
 	# Initially style the button
-	if Engine.editor_hint and fresh:
+	if Engine.is_editor_hint() and fresh:
 		self.style = load("res://addons/color_picker_text_button/styleboxflat.tres")
-		rect_position = Vector2(20, 10)
+		position = Vector2(20, 10)
 
 	setup_label()
 
@@ -60,7 +61,8 @@ func setup_label():
 	label = Label.new()
 	center.add_child(label)
 	if custom_font:
-		label.set("custom_fonts/font", custom_font)
+		label.set("theme_override_fonts/font", custom_font)
+	label.set("theme_override_font_sizes/font_size", font_size)
 	call_deferred("init_size")
 	label.text = text_value
 	text = ""
@@ -76,32 +78,32 @@ func show_hide_label(show_text):
 
 func rect_changed():
 	if center:
-		center.rect_size = rect_size
+		center.size = size
 
 
 func init_size():
-	if Engine.editor_hint and fresh:
-		rect_size = Vector2(200, 60)
+	if Engine.is_editor_hint() and fresh:
+		size = Vector2(200, 60)
 		fresh = false
 	rect_changed()
 
 
 func set_font(font):
 	custom_font = font
-	if Engine.editor_hint and label:
-		label.set("custom_fonts/font", font)
+	if Engine.is_editor_hint() and label:
+		label.set("theme_override_fonts/font", font)
 
 
 func set_styles(s):
 	style = s
 	if style == null:
-		style = get("custom_styles/normal")
+		style = get("theme_override_styles/normal")
 		if style == null:
 			return # Stick with default button style
 	else:
-		set("custom_styles/normal", style)
-	var next_style = set_style(style, "custom_styles/hover")
-	next_style = set_style(next_style, "custom_styles/pressed")
+		set("theme_override_styles/normal", style)
+	var next_style = set_style(style, "theme_override_styles/hover")
+	next_style = set_style(next_style, "theme_override_styles/pressed")
 
 
 func set_style(current_style, path):
@@ -116,7 +118,7 @@ func set_style(current_style, path):
 func set_label_text(txt):
 	text_value = txt
 	text = ""
-	if Engine.editor_hint and label:
+	if Engine.is_editor_hint() and label:
 		# Export setter is called before the _ready() function when running scene.
 		label.text = txt
 
@@ -135,29 +137,29 @@ func panel_closed():
 
 func set_colors_in_tool(c):
 	color = c
-	if Engine.editor_hint and label:
+	if Engine.is_editor_hint() and label:
 		set_colors(c)
 
 
 func update_light_text_color(c):
 	light_text_color = c
-	if Engine.editor_hint and label:
+	if Engine.is_editor_hint() and label:
 		set_colors(color)
 
 
 func update_dark_text_color(c):
 	dark_text_color = c
-	if Engine.editor_hint and label:
+	if Engine.is_editor_hint() and label:
 		set_colors(color)
 
 
 func set_colors(c):
-	if not Engine.editor_hint:
+	if not Engine.is_editor_hint():
 		emit_signal("color_changed", c)
 	self_modulate = c
 	color = c
 	var grey_value = (c.r + c.g + c.b) / 3
 	if grey_value > text_color_flip_threshold:
-		label.set("custom_colors/font_color", dark_text_color)
+		label.set("theme_override_colors/font_color", dark_text_color)
 	else:
-		label.set("custom_colors/font_color", light_text_color)
+		label.set("theme_override_colors/font_color", light_text_color)
--- addons/color_picker_text_button/plugin.gd
+++ addons/color_picker_text_button/plugin.gd
@@ -1,4 +1,4 @@
-tool
+@tool
 extends EditorPlugin
 
 const TYPE_NAME = "ColorPickerTextButton"
